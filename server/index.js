f0ac5dcd2ac3b21d361be742ef6a14118db7d256
Patrick Runkel <patrickrunkel86@yahoo.com>
2025-04-12 00:30:11 +0200

    beta 1.0

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// statische Dateien
+app.use(express.static(path.join(__dirname, "../public")));
+
+const cities = ["CGN-Raves Live Chat"];
+
+const chatRooms = {};
+
+io.on("connection", (socket) => {
+  console.log("Ein Nutzer ist verbunden");
+
+  socket.on("joinCity", (city) => {
+    if (!cities.includes(city)) return;
+    socket.join(city);
+    socket.city = city;
+    io.to(city).emit("chat message", `📢 Neuer Nutzer im ${city}-Chat`);
+  });
+
+  socket.on("chat message", (msg) => {
+    if (socket.city) {
+      io.to(socket.city).emit("chat message", msg);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.city) {
+      io.to(socket.city).emit("chat message", "🚪 Nutzer hat den Chat verlassen");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
